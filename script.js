83016537925765b978544986b0997603b55a249b
RSV34 <150009063+RSV34@users.noreply.github.com>
2024-04-09 14:29:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-console.log(document.documentElement.clientHeight);
-console.log(document.documentElement.clientWidth);
-
-// console.log(document.body);
-const ele2 = document.getElementsByClassName('image_a1')[0];
-const ele3 = document.getElementsByClassName('image_a2')[0];
-const ele1 = document.getElementsByClassName('image_a4')[0];
-
-
-console.log(ele2.width);
-console.log(ele3.width);
-console.log(ele1.width);
 
 const nav_cross = document.getElementsByClassName('a3')[0];
 const a = document.getElementsByClassName('a')[0];
+const b = document.getElementsByClassName('banner1')[0];
+const btn1 = document.getElementsByClassName('btn1')[0];
+
+btn1.onclick = function(){
+    document.getElementsByTagName('body')[0].style.overflow='hidden';
+    // console.log(b.classList);
+}
 
 nav_cross.onclick = function(){
     a.classList.toggle('show');
-    console.log(a.classList);
+    document.getElementsByTagName('body')[0].style.overflow='scroll';
+    // console.log(a.classList);
+    // console.log(b.classList);
 }
 
